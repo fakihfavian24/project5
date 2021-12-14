ceea6a6f854a1f77ec77ad16343b43041380530b
fakihfavian24 <76416222+fakihfavian24@users.noreply.github.com>
2021-12-14 20:49:45 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "103.31.39.50";
+$username = "user_project5db";
+$password = "user_project5db";
+$dbname = "project5db";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "berhasil";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <img src="index.jpg" alt="">
+</body>
+
+</html>
